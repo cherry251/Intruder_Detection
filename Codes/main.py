031dd8ccdac77817f2a10b28b421cc1ec24f86f0
Charitha Dhananjaya <charitha.dhananjaya464@gmail.com>
2018-09-23 01:57:36 +0530

    Data read done... 1.) Data read to a df from the excel file. 2.) Then a np.nd array created.

--- Codes/main.py
+++ Codes/main.py
@@ -1,9 +1,12 @@
+import numpy as np
 import pandas as pd
 
-Signal_df = pd.read_excel("D:\Academics\EE 405\August Start\Intruder Detection\MLCode\DataFiles\RawSignals.xlsx", sheet_name=0, header=0)
+Signal_df = pd.read_excel("D:\Academics\EE 405\August Start\Intruder Detection\MLCode\DataFiles\RawSignals.xlsx",
+                          sheet_name=0)
+lengths = list(Signal_df)
 
 for i in range(31):
-    SignalData = Signal_df.iloc[0:188, i]
-    # print(SignalData)
+    SignalData = Signal_df.iloc[0:260, i].values
 
-print(type(SignalData))
+
+
