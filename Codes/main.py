49d91003f60600d6f7ddf59c7f248309b00aedda
Charitha Dhananjaya <charitha.dhananjaya464@gmail.com>
2018-09-23 01:30:46 +0530

    Checked...

--- Codes/main.py
+++ Codes/main.py
@@ -1,1 +1,9 @@
-print("Hello World...!")
+import pandas as pd
+
+Signal_df = pd.read_excel("D:\Academics\EE 405\August Start\Intruder Detection\MLCode\DataFiles\RawSignals.xlsx", sheet_name=0, header=0)
+
+for i in range(31):
+    SignalData = Signal_df.iloc[0:188, i]
+    # print(SignalData)
+
+print(type(SignalData))
