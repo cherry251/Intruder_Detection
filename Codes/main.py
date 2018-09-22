ffc9a156285a18fd47cef7e863dfa1d035c9a7e1
Charitha Dhananjaya <charitha.dhananjaya464@gmail.com>
2018-09-23 02:35:30 +0530

    Plots are shown.... Works as Expected...

--- Codes/main.py
+++ Codes/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from pylab import figure, plot, show
 
 Signal_df = pd.read_excel("D:\Academics\EE 405\August Start\Intruder Detection\MLCode\DataFiles\RawSignals.xlsx",
                           sheet_name=0)
@@ -7,6 +8,9 @@ lengths = list(Signal_df)
 
 for i in range(31):
     SignalData = Signal_df.iloc[0:260, i].values
+    figure(i)
+    plot(SignalData)
+    show()
 
 
 
