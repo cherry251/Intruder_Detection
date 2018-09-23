edbb3e8edee29c688eccef3b54156bc75d31953e
Charitha Dhananjaya <charitha.dhananjaya464@gmail.com>
2018-09-23 14:15:31 +0530

    Corrected the FIR Filtering... 1.) Sample rate of the filter 100Hz 2.) Ripple dB is 20dB 3.) Cutoff frequency is 15Hz 4.) Taps are scale 0.5

--- Codes/main.py
+++ Codes/main.py
@@ -1,19 +1,22 @@
 import numpy as np
 import pandas as pd
 from pylab import figure, plot, show
+from myfunctions import filter_fir, signal_arrange
 
 Signal_df = pd.read_excel("D:\Academics\EE 405\August Start\Intruder Detection\MLCode\DataFiles\RawSignals.xlsx",
                           sheet_name=0)
 lengths = list(Signal_df)
 
 for i in range(31):
-    SignalData = Signal_df.iloc[0:260, i].values
-
-
-
+    SignalData = Signal_df.iloc[0:188, i]
+    newSignal = signal_arrange(SignalData)
+    FilterSignal = filter_fir(newSignal)
+    print(type(newSignal))
     figure(i)
-    plot(SignalData)
+    plot(newSignal)
+    plot(FilterSignal)
     show()
+
 
 
 
--- Codes/myfunctions.py
+++ Codes/myfunctions.py
@@ -6,11 +6,11 @@ def filter_fir(signal_data):  # Design of FIR Filter for Smoothing the Signal
     sample_rate = 110.0  # Sample Rate keep as 110Hz because Data Sample Rate is 50 Hz
     nyq_rate = sample_rate / 2.0
 
-    width = 5.0 / nyq_rate
+    width = 10.0 / nyq_rate
     ripple_db = 20.0
     n, beta = kaiserord(ripple_db, width)
 
-    cutoff_hz = 10.0
+    cutoff_hz = 15.0
     taps = firwin(n, cutoff_hz / nyq_rate, window=('kaiser', beta))
     filtered_signal = lfilter(taps, 0.5, signal_data)
 
@@ -19,15 +19,12 @@ def filter_fir(signal_data):  # Design of FIR Filter for Smoothing the Signal
 
 def signal_arrange(raw_signal):
 
-
-    sample_rate = 50
     sensitivity = 0.000125  # V/bit | Convert to a Voltage Signal
 
     raw_signal = [ele*sensitivity for ele in raw_signal]
-
     mean = sum(raw_signal)/len(raw_signal)
 
     # Removing the Mean from the Signal
-    new_signal = raw_signal - mean
+    new_signal = [ele-mean for ele in raw_signal]
 
     return new_signal
