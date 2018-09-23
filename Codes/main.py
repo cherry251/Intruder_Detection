42c698952adb071629215d6a0ed7153d8aa64c7e
Charitha Dhananjaya <charitha.dhananjaya464@gmail.com>
2018-09-23 11:24:54 +0530

    Generated the functions for 1.) Signal convertion to voltage levels. 2.) FIR filter for noise reduction

--- Codes/main.py
+++ Codes/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from pylab import figure, plot, show
+from functions import filter_fir, signal_arrange
 
 Signal_df = pd.read_excel("D:\Academics\EE 405\August Start\Intruder Detection\MLCode\DataFiles\RawSignals.xlsx",
                           sheet_name=0)
@@ -8,6 +9,9 @@ lengths = list(Signal_df)
 
 for i in range(31):
     SignalData = Signal_df.iloc[0:260, i].values
+
+
+
     figure(i)
     plot(SignalData)
     show()
